5796ecb0b5321381595ce34fda2c3bdc774ee7d0
melizasosa <s.m.meliza@gmail.com>
2019-09-16 18:17:56 -0500

    instalando CLI

---
+++ src/file.js
@@ -0,0 +1,22 @@
+// Contiene utilidads para la manipulación y trasnformacion de arhivos
+const fs = require('fs');
+
+const  filePathExists=(filePath)=> {
+  return new Promise((resolve, reject) => {
+    fs.stat(filePath, (err, stats) => {
+      if (err && err.code === 'ENOENT') {
+        console.log(false);
+        return resolve(false);
+      } else if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      if (stats.isFile() || stats.isDirectory()) {
+        console.log(true);
+        return resolve(true);
+      }
+    });
+  });
+};
+
+filePathExists('prueba\\archivo.md') 
