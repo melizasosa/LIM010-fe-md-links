66415e0388b0a95df39a2389c5e71e8a9b9ee327
melizasosa <s.m.meliza@gmail.com>
2019-09-17 07:30:38 -0500

    Fx de conocer extension del archivo y test 100%

--- mylib/src/file.js
+++ mylib/src/file.js
@@ -1,22 +1,37 @@
 // Contiene utilidads para la manipulación y trasnformacion de arhivos
 const fs = require('fs');
+const path = require('path');
 
-export const  filePathExists=(filePath)=> {
-  return new Promise((resolve, reject) => {
-    fs.stat(filePath, (err, stats) => {
-      if (err && err.code === 'ENOENT') {
-        console.log(false);
-        return resolve(false);
-      } else if (err) {
-        console.log(err);
-        return reject(err);
-      }
-      if (stats.isFile() || stats.isDirectory()) {
-        console.log(true);
-        return resolve(true);
-      }
-    });
-  });
+// export const filePathExists = (nameFile) => {
+//   fsModule.stat(nameFile, (err, data)=> {
+//     if (err) 
+//       console.log('no existe' + err);
+//     else 
+//     console.log(data);
+//     console.log(data.isFile()); 
+//   });
+// }
+
+
+// // VERIFICAR SI SON ARCHIVOS
+// export const  filePathExists=(filePath)=> {
+//   return new Promise((resolve, reject) => {
+//     fs.stat(filePath, (err, stats) => {
+//       if (stats.isFile()){
+//         console.log(true);
+//         return resolve(true);
+//       } else {
+//         console.log(err);
+//         return reject(false);
+//       } 
+//     });
+//   });
+// };
+
+// filePathExists('prueba\\archivo.md') 
+
+// OBTENER SÓLO LOS ARCHIVOS CON EXTENSIÓN .MD
+export const isFileMd = file => {
+  return path.extname(file);
 };
 
-filePathExists('prueba\\archivo.md') 
--- mylib/test/test.spec.js
+++ mylib/test/test.spec.js
@@ -1,29 +1,41 @@
 import {functionTypePath} from '../src/path.js';
-import {filePathExists} from '../src/file.js';
+import {isFileMd} from '../src/file.js';
+import path from 'path';
+
 describe('Test de la función convertir ruta relativa', () => {
   it('Debería ser una función', () => {
   	expect(typeof functionTypePath).toBe('function');
 	});	
   it('Debería convertir una ruta relativa a absoluta', () => {
-    expect(functionTypePath('prueba/archivo.md')).toBe('C:\\Users\\L-67\\Desktop\\Proyecto-Links\\LIM010-fe-md-links\\prueba\\archivo.md');
+    expect(functionTypePath('prueba/archivo.md')).toBe(path.join(process.cwd(),'prueba\\archivo.md'));
   });
   it('Debería retornar una ruta absoluta', () => {
-    expect(functionTypePath('C:\\Users\\L-67\\Desktop\\Proyecto-Links\\LIM010-fe-md-links\\prueba\\archivo.md')).toBe('C:\\Users\\L-67\\Desktop\\Proyecto-Links\\LIM010-fe-md-links\\prueba\\archivo.md');
+    //let pathAbsolute = path.join(process.cwd(), 'prueba\\archivo.md');
+    expect(functionTypePath(path.join(process.cwd(),'prueba\\archivo.md'))).toBe(path.join(process.cwd(),'prueba\\archivo.md'));
 	});
 });
 
-describe('Permite verificar si es un archivo', () => {
+// describe('Permite verificar si es un archivo', () => {
+//   it('Debería ser una función', () => {
+//   	expect(typeof filePathExists).toBe('function');
+// 	});	
+//   it('Debería ser true para una ruta que sea de archivo', () => {
+//     return filePathExists('prueba\\archivo.md').then(result => {
+//       expect(result).toBe(false)
+//     });
+//   });
+//   it('Debería ser false para una ruta que no sea de archivo', () => {
+//     return filePathExists('prueba\\archivoo.md').c(result => {
+//       expect(result).toBe(true)
+//     });
+//   });
+// });
+
+describe('Permite conocer la extension del archivo', () => {
   it('Debería ser una función', () => {
-  	expect(typeof filePathExists).toBe('function');
+  	expect(typeof isFileMd).toBe('function');
 	});	
-  it('Debería ser true para una ruta que sea de archivo', () => {
-    return filePathExists('prueba\\archivo.md').then(result => {
-      expect(result).toBe(true)
-    });
-  });
-  it('Debería ser false para una ruta que no sea de archivo', () => {
-    return filePathExists('prueba\\archivoo.md').then(result => {
-      expect(result).toBe(false)
-    });
+  it('Tipo de archivo', () => {
+    expect(isFileMd('prueba/archivo.md')).toBe('.md');
   });
 });
