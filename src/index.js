2990b0f73bf8bed84b9cd9bc0584962bce124b43
melizasosa <s.m.meliza@gmail.com>
2019-09-18 19:06:00 -0500

    Avance de fx extraer links

--- src/index.js
+++ src/index.js
@@ -1,47 +1,72 @@
 // Contiene utilidads para la manipulación y trasnformacion de arhivos
-const fs = require('fs');
+const fsModule = require('fs');
 const pathModule = require('path');
+const marked = require('marked');
 
-export const functionTypePath = (inputPath) => {
-  if (!pathModule.isAbsolute(inputPath)) {
-    return pathModule.resolve(inputPath);
+export const functionTypePath = (filePath) => {
+  if (!pathModule.isAbsolute(filePath)) {
+    return pathModule.resolve(filePath);
   }
-  return inputPath;
+  return filePath;
 };
 
 
 // VERIFICAR SI ES ARCHIVO
 export const functionFilePathExists = (nameFile) => {
-  const isItFile = fs.statSync(nameFile);
+  const isItFile = fsModule.statSync(nameFile);
   return isItFile.isFile();
 };
 
 // OBTENER SÓLO LOS ARCHIVOS CON EXTENSIÓN .MD
 export const functionIsFileMd = (file) => {
-  const extName = pathModule.extname(file) ==='.md';
+  const extName = pathModule.extname(file) === '.md';
   return extName;
-}
+};
 
 // LEE SINCRONA TODO EL CONTENIDO  DE UN ARCHIVO
 export const functionReadFileS = (file) => {
-  let data = fs.readFileSync(file, 'utf8');
-  console.log(data);
+  const data = fsModule.readFileSync(file, 'utf8');
   return data;
-}
+};
 
 //  RECORRE TODO LOS ARCHIVOS CON EXT .MD  DE LA CARPETA Y LO GUARDA EN ARRAY
 export const functionReadAllFiles = (route) => {
-  let array = [];  
+  let arrayFileMd = [];
   if (functionFilePathExists(route)) {
     if (functionIsFileMd(route)) {
-      array.push(route);
+      arrayFileMd.push(route);
     }
   } else {
-    let dir = fs.readdirSync(route);
-    dir.forEach((file) => {
-      let arrayNew = functionReadAllFiles(pathModule.join(route, file));
-      array = array.concat(arrayNew);
-    });     
+    const directory = fsModule.readdirSync(route);
+    directory.forEach((file) => {
+      const arrayPath = functionReadAllFiles(pathModule.join(route, file));
+      arrayFileMd = arrayFileMd.concat(arrayPath);
+    });
   }
-  return array;  
+  return arrayFileMd;
 };
+// console.log(functionReadAllFiles('prueba'));
+
+// RECORRER Y LEER LOS LINKS DE ARCHIVOS .MD
+export const functionReadLinkFile = (arrayFileMd) => {
+  const arrObj = [];
+  const arrayFile = functionReadAllFiles(functionTypePath(arrayFileMd));
+  arrayFile.forEach((filePath) => {
+    let linksFileMd = functionReadFileS(filePath);
+    const renderer = new marked.Renderer();
+    renderer.link = (href, title, text) => {
+      arrObj.push(
+        { href: href,
+          text:text,
+          filepath: filePath,
+        }); 
+      return renderer;
+    };
+    marked(filePath, { renderer:renderer});
+  });
+  // eslint-disable-next-line no-undef
+  return arrObj;
+};
+// const md = 'this is some example markdown with [a link](github.com).';
+
+console.log(functionReadLinkFile('C:/Users/L-67/Desktop/Proyecto-Links/LIM-010-fe-md-links/prueba'));
